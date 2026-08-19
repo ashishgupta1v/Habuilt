0ec2ab608a39ebe55e81a4cbdb3fba9959db51f2
Ashish Gupta <ashishgupta1v@gmail.com>
2026-08-19 23:49:51 +0530

    feat(pwa): zero horizontal scroll across streaks, milestones, bottom nav & enable pinch zoom

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,8 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name', 'Habuilt') }}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes, viewport-fit=cover">
+        <title inertia>{{ config('app.name', 'Habuilt') }}</title>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=20260418">
         <link rel="shortcut icon" href="/favicon.svg?v=20260418">
         <link rel="preconnect" href="https://fonts.googleapis.com">
